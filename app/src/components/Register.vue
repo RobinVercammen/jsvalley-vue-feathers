cd26fa6f92d335c29fba64aa21c682c719f12611
Robin Vercammen <robinvercammen1993@hotmail.com>
2017-06-03 11:42:53 +0200

    register component

---
+++ app/src/components/Register.vue
@@ -0,0 +1,25 @@
+<template>
+  <form v-on:submit.prevent="createUser">
+    <div>
+      <input name="email-form" type="email">
+    </div>
+    <div>
+      <input name="password-form" type="password">
+    </div>
+    <div>
+      <button type="submit">Creat user</button>
+    </div>
+  </form>
+</template>
+<script>
+export default {
+  methods: {
+    createUser($event) {
+      const email = $event.target['email-form'].value;
+      const password = $event.target['password-form'].value;
+      
+    },
+  },
+};
+
+</script>
