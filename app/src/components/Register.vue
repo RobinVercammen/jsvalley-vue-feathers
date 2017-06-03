35c2a901bfe1da23ffd3314f6271825acab2b450
Robin Vercammen <robinvercammen1993@hotmail.com>
2017-06-03 12:00:43 +0200

    use feathers client

--- app/src/components/Register.vue
+++ app/src/components/Register.vue
@@ -12,12 +12,20 @@
   </form>
 </template>
 <script>
+import feathers from 'feathers-client';
+
+
 export default {
   methods: {
     createUser($event) {
       const email = $event.target['email-form'].value;
       const password = $event.target['password-form'].value;
-      
+
+      const client = feathers()
+        .configure(feathers.hooks())
+        .configure(feathers.rest('http://localhost:3030').fetch(window.fetch.bind(window)));
+      const users = client.service('users');
+      users.create({ email, password }).then(console.log, console.log);
     },
   },
 };
